8c9c08391ceb55b162ef457ac1118325a9135663
Lilian Burdianov <lilian.burdianov@gmail.com>
2018-12-02 15:08:03 +0400

    Update tests, create views, etc...

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -10,10 +10,22 @@ class ThreadsTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
-    public function a_user_can_browse_threads()
+    public function a_user_can_view_all_threads()
     {
+        $thread = factory('App\Thread')->create();
+
         $response = $this->get('/threads');
 
-        $response->assertStatus(200);
+        $response->assertSee($thread->title);
+//        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/' . $thread->id);
+        $response->assertSee($thread->title);
     }
 }
